08073e475613c403c3624974d4bc5dd4e07269d9
Brayden Allen <allenbrayden@gmail.com>
2019-03-31 21:24:27 -0600

    finished assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 import './App.css';
 
+import Post from './Post/Post'
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+
+const baseUrl = "https://practiceapi.devmountain.com/api/"
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      posts: []
+      posts: [],
     };
 
     this.updatePost = this.updatePost.bind( this );
@@ -19,19 +22,43 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    axios.get(`${baseUrl}/posts`)
+    .then(res => { 
+      // console.log("POST IS WORKING", res)
+      this.setState({
+        posts: res.data
+      })
+    })
+    .catch(res => {
+      console.log('ERRORRRRRR', res)
+    })
   }
 
-  updatePost() {
+  updatePost(id, text) {
+    console.log('PUT IS WORKING')
+    axios.put(`${baseUrl}/posts?id=${id}`, {text})
+    .then(res => { 
+      this.setState({
+        posts: res.data 
+      })
+    })
+    .catch(res => {
+      console.log('ERRORRRRRR', res)
+    })
+  }
   
-  }
-
+  
   deletePost() {
-
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${ id }`).then( results => {
+      this.setState({ posts: results.data });
+    });
   }
-
+  
   createPost() {
-
+    // axios.post(`${baseUrl}`/posts)
+    axios.post('https://practiceapi.devmountain.com/api/posts', { text }).then( results => {
+      this.setState({ posts: results.data });
+    });
   }
 
   render() {
@@ -43,7 +70,13 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
+          <Compose/>
+          {posts.map((el)=> <Post updatePostFn={this.updatePost} 
+                                  deletePostFn={ this.deletePost }
+                                  id={el.id} 
+                                  text={el.text} 
+                                  date={el.date} 
+                                  key={el.id}/>)}
           
         </section>
       </div>
